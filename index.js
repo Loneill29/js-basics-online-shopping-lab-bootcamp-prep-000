20ef5b45e4e11e707b95113b6d161945db76adf7
Linda ONeill <oneill.linda21@gmail.com>
2017-11-16 17:09:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,12 +30,15 @@ for (let i = 0; i < l; i++) {
 
 while (itemsAndPrices.length) {
 
-      itemsAndPrices = itemsAndPrices.join(" and ");
+  case 1:
+break;
+case 2:
+itemsAndPrices = itemsAndPrices.join(" and ");
+break;
+default:
+itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+itemsAndPrices = itemsAndPrices.join(", ");
+}
 
-
-      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
-      itemsAndPrices = itemsAndPrices.join(", ");
-  }
-
-  console.log(`In your cart, you have ${itemsAndPrices}.`);
+console.log(`In your cart, you have ${itemsAndPrices}.`);
 }
